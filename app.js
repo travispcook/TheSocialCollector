90fc22d7c9dccd219ab31a8a01a8cff682a502ee
travispcook <travispcook@Traviss-MacBook-Pro.local>
2013-12-05 17:54:27 -0700

    cleaned up a few things, FB posts feeding, G+ disabled

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ var express = require('express')
   , passport = require('passport')
   , util = require('util')
   , request = require('request')
+  , GoogleStrategy = require('passport-google').Strategy
   , FacebookStrategy = require('passport-facebook').Strategy;
 
+//Facebook IDs
 var FACEBOOK_APP_ID = "662814087096037";
 var FACEBOOK_APP_SECRET = "a493b80dafb23c0081b88e91b99e956c";
 var FB = require('fb');
+
+//Google IDs
+
 
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -17,6 +22,8 @@ passport.deserializeUser(function(obj, done) {
 });
 
 
+//Facebook auth setup
+
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
@@ -25,18 +32,34 @@ passport.use(new FacebookStrategy({
   function(accessToken, refreshToken, profile, done) {
    profile.accessToken = accessToken;
     process.nextTick(function () {
-     //creates user
+     //creates user for Facebook
       return done(null, profile);
     });
   }));
+
 /*
 passport.use(new FacebookStrategy({
     profileFields: ['id', 'displayName', 'photos', 'pic_square']
   }));
 */
+
+
+//Google auth setup
+passport.use(new GoogleStrategy({
+    returnURL: 'http://www.thesocialcollector.com/auth/google/return',
+    realm: 'http://www.thesocialcollector.com/'
+  },
+  function(identifier, profile, done) {
+    process.nextTick(function () {
+      profile.identifier = identifier;
+      return done(null, profile);
+    });
+  }
+));
+
 var app = express();
 
-// configure Express
+// Configure Express
 app.configure(function() {
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
@@ -65,7 +88,7 @@ app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
 
-
+//facebook auth
 app.get('/auth/facebook',
   passport.authenticate('facebook', { scope:
     ['user_friends', 'user_likes', 'user_events', 'user_status',
@@ -74,25 +97,44 @@ app.get('/auth/facebook',
     }),
   function(req, res){
   });
+app.get('/auth/facebook',
+  passport.authenticate('facebook', { display: 'popup' }));
 
 
+//facebook auth callback
 app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
   });
 
+
+//Google auth
+app.get('/auth/google',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+//Google auth callback(return)
+app.get('/auth/google/return',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+
 app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
 });
 
-app.get('/auth/facebook',
-  passport.authenticate('facebook', { display: 'popup' }));
 
-app.get('/feeds', ensureAuthenticated, function(req, res){
+//pull facebook home feed
+
+app.get('/facebook/feeds', ensureAuthenticated, function(req, res){
   var accessToken = req.user.accessToken;
-  var feed_url = 'https://graph.facebook.com/me/home?access_token='+accessToken+'&limit=3';
+  var feed_url = 'https://graph.facebook.com/me/home?access_token='+accessToken+'&limit=15';
   var http = require('http');
   var https = require('https');
 
@@ -105,8 +147,28 @@ app.get('/feeds', ensureAuthenticated, function(req, res){
     res.send(feedItems);
     }
   });
-  
 });
+
+//Google home feed conflicting with facebook OAuth....will resolve
+
+// app.get('/google/feeds', ensureAuthenticated, function(req, res){
+//   var accessToken = req.user.accessToken;
+//   var feed_url = 'https://www.googleapis.com/auth/plus.stream.read';
+//   var http = require('http');
+//   var https = require('https');
+
+
+//   request.get(feed_url, function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     var feedItems = JSON.parse(body);
+
+//     console.log(feedItems); // Print the web page.
+//     res.send(feedItems);
+//     }
+//   });
+// });
+
+
 
 var port = 80;
 app.listen(port);
--- public/js/controller.js
+++ public/js/controller.js
@@ -3,11 +3,11 @@ var FBApp = angular.module('FBApp',[]);
 
 //controller
 FBApp.controller('FBController', function ($scope, $http){
-	var feed = '/feeds';
+	var feed = '/facebook/feeds';
 	$http.get(feed).success(function(feedItems) {
 		
 		console.log(feedItems);
-		$scope.FBfeed = feedItems;
+		$scope.FBfeed = feedItems.data;
 		
 	});
 $scope.search = '';
